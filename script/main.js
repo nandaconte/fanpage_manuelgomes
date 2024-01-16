7e5b068dce907705e5fc6dd9d1eda641957af071
nandaconte <fernanda.accsilva@senacsp.edu.br>
2024-01-16 13:07:17 -0300

    carrossel feio

--- script/main.js
+++ script/main.js
@@ -1,3 +1,16 @@
-const img = [
-    "img src="
-]
+let count = 1
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 2000);
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count=1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
+
